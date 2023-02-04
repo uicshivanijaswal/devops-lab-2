45bd95aebaa6f4c8161e09bfc4b00128f973e2ca
edit <uic.shivanijaswal@gmail.com>
2023-02-04 17:09:16 +0000

    First commit again

--- script.sh
+++ script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+rm -R fol_1
+rm -R fol_2
 mkdir fol_1
 mkdir fol_2
 cd fol_1
@@ -14,3 +16,16 @@ touch .gitignore
 echo ".gitignore" >> .gitignore
 echo fol_1 >> .gitignore
 echo fol_2 >> .gitignore
+chmod 600 fol_1/1_1.txt
+chmod 600 fol_1/1_3.txt
+chmod 600 fol_2/2_1.txt
+chmod 600 fol_2/2_3.txt
+chmod 777 fol_1/1_2.txt
+chmod 777 fol_1/1_2.txt
+touch README.md
+echo "# Week 2 Lab – Bash & Git" > README.md
+echo Shivani Jaswal >> README.md
+echo "[Devops lab 2](https://github.com/uicshivanijaswal/devops-lab-2)" >> README.md
+echo --- >> README.md
+echo "To run the script.sh run: \`bash script.sh\`" >> README.md
+echo Job Completed
